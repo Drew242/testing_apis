9071f9ad069fb167fbecdf76e2f20b10c84cc2e6
Drew Conly <atconly@gmail.com>
2015-09-15 09:53:21 -0600

    make third test pass for create in items controller

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -21,4 +21,12 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
     assert_equal "Lanister",           item[:name]
     assert_equal "My uncle is my dad", item[:description]
   end
+
+  test "#create" do
+    item_params = { name: "Arya", description: "Is not blind" }
+
+    post :create, format: :json, item: item_params
+
+    assert_response :success
+  end
 end
